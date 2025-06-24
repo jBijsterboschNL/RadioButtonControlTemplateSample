c7c94d8444459134b4beae99abee3878fedf3ae5
John Bijsterbosch <john.bijsterbosch@live.com>
2025-06-24 15:35:41 +0200

    Added radio button sample with and without custom control template

--- RadioButtonControlTemplateSample/CheckedColorConverter.cs
+++ RadioButtonControlTemplateSample/CheckedColorConverter.cs
@@ -1,6 +1,19 @@
+using System.Globalization;
+
 namespace RadioButtonControlTemplateSample;
 
-public class CheckedColorConverter
+public class CheckedColorConverter : IValueConverter
 {
-    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+            return Colors.Black;
+        
+        return boolValue ? Colors.Green : Colors.Red;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
--- RadioButtonControlTemplateSample/Platforms/iOS/AppDelegate.cs
+++ RadioButtonControlTemplateSample/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace RadioButtonControlTemplateSample;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
